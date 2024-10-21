a3a1ee8a3ec456d7a05aa02942e05861a9bf0433
raychaser <terminadev31@gmail.com>
2024-10-21 13:34:24 +0400

    add methods for drawing some primitives

---
+++ src/Charset.cs
@@ -0,0 +1,43 @@
+namespace raychLib;
+
+public enum ExtraCharset
+{
+	SmileOutline = '☺',
+	SmileFilled = '☻',
+
+	ArrowUp = '▲',
+	ArrowDown = '▼',
+	ArrowLeft = '◄',
+	ArrowRight = '►',
+
+	LineArrowUp = '↑',
+	LineArrowDown = '↓',
+	LineArrowLeft = '←',
+	LineArrowRight = '→',
+
+	Gear = '☼',
+	
+	CopyrightSign = '©',
+
+	CheckMark = '✓',
+
+	BlockFull = '█',
+	BlockMedium = '▓',
+	BlockLight = '▒',
+
+	BorderLightHorizontal = '─',
+	BorderLightVertical = '│',
+	BorderLightNorthWestCorner = '┌',
+	BorderLightNorthEastCorner = '┐',
+	BorderLightSouthWestCorner = '└',
+	BorderLightSouthEastCorner = '┘',
+
+	BorderBoldHorizontal = '═',
+	BorderBoldVertical = '║',
+	BorderBoldNorthWestCorner = '╔',
+	BorderBoldNorthEastCorner = '╗',
+	BorderBoldSouthWestCorner = '╚',
+	BorderBoldSouthEastCorner = '╝',
+
+
+}
--- src/TerminalRenderer.Primitives.cs
+++ src/TerminalRenderer.Primitives.cs
@@ -20,4 +20,71 @@ public partial class TerminalRenderer
 			this.DrawGlyph((int)(startX + dirX * i), (int)(startY + dirY * i), glyph);
 		}
 	}
+
+	public void DrawRectangleLines(int positionX, int positionY, int width, int height, TerminalGlyph glyph)
+	{
+		this.DrawLine(positionX, positionY, positionX, positionY + height, glyph);
+		this.DrawLine(positionX, positionY + height, positionX + width, positionY + height, glyph);
+		this.DrawLine(positionX + width, positionY + height, positionX + width, positionY, glyph);
+		this.DrawLine(positionX + width, positionY, positionX, positionY, glyph);
+	}
+
+	public void DrawRectangleFilled(int positionX, int positionY, int width, int height, TerminalGlyph glyph)
+	{
+		for (int i = 0; i < height; i++)
+		{
+			for (int j = 0; j < width; j++)
+			{
+				DrawGlyph(positionX + j, positionY + i, glyph);
+			}
+		}
+	}
+
+	public void DrawFrame(int positionX, int positionY, int width, int height, BorderThickness thick, TerminalColor foregroundColor, TerminalColor backgroundColor)
+	{
+		var horGlyph = new TerminalGlyph(
+				(char)(thick == BorderThickness.Light ? ExtraCharset.BorderLightHorizontal : ExtraCharset.BorderBoldHorizontal),
+				foregroundColor,
+				backgroundColor);
+		var verGlyph = new TerminalGlyph(
+				(char)(thick == BorderThickness.Light ? ExtraCharset.BorderLightVertical : ExtraCharset.BorderBoldVertical),
+				foregroundColor,
+				backgroundColor);
+		var nwCorner = new TerminalGlyph(
+				(char)(thick == BorderThickness.Light ? ExtraCharset.BorderLightNorthWestCorner : ExtraCharset.BorderBoldNorthWestCorner),
+				foregroundColor,
+				backgroundColor);
+		var neCorner = new TerminalGlyph(
+				(char)(thick == BorderThickness.Light ? ExtraCharset.BorderLightNorthEastCorner : ExtraCharset.BorderBoldNorthEastCorner),
+				foregroundColor,
+				backgroundColor);
+		var swCorner = new TerminalGlyph(
+				(char)(thick == BorderThickness.Light ? ExtraCharset.BorderLightSouthWestCorner : ExtraCharset.BorderBoldSouthWestCorner),
+				foregroundColor,
+				backgroundColor);
+		var seCorner = new TerminalGlyph(
+				(char)(thick == BorderThickness.Light ? ExtraCharset.BorderLightSouthEastCorner : ExtraCharset.BorderBoldSouthEastCorner),
+				foregroundColor,
+				backgroundColor);
+		this.DrawLine(positionX, positionY, positionX, positionY + height, verGlyph);
+		this.DrawLine(positionX, positionY + height, positionX + width, positionY + height, horGlyph);
+		this.DrawLine(positionX + width, positionY + height, positionX + width, positionY, verGlyph);
+		this.DrawLine(positionX + width, positionY, positionX, positionY, horGlyph);
+		this.DrawGlyph(positionX, positionY, nwCorner);
+		this.DrawGlyph(positionX + width, positionY, neCorner);
+		this.DrawGlyph(positionX, positionY + height, swCorner);
+		this.DrawGlyph(positionX + width, positionY + height, seCorner);
+	}
+
+	public void DrawFrameFilled(int positionX, int positionY, int width, int height, BorderThickness thick, TerminalColor foregroundColor, TerminalColor backgroundColor)
+	{
+		DrawRectangleFilled(positionX, positionY, width, height, new TerminalGlyph((char)ExtraCharset.BlockFull, backgroundColor, backgroundColor));
+		DrawFrame(positionX, positionY, width, height, thick, foregroundColor, backgroundColor);
+	}
+}
+
+public enum BorderThickness
+{
+	Light = 0,
+	Bold = 1
 }
